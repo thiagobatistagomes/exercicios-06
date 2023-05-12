cc3e9e1fdb2c23940813097b30d15ec83bbb7c99
thiagobatistagomes <122994560+thiagobatistagomes@users.noreply.github.com>
2023-05-12 14:56:04 -0300

    ex05 corrigido

--- exercicio05.c
+++ exercicio05.c
@@ -4,16 +4,23 @@ int main ()
 {
     int qtdmul, num, i, multiplo;
 
-    printf("Digite quantos multiplos deseja ver:\n");
-    scanf("%d", &qtdmul);
+    do
+    {
+        printf("Digite quantos multiplos deseja ver:\n");
+        scanf("%d", &qtdmul);
+    } while (qtdmul < 0);
+    
+    
 
     printf("Digite o numero que deseja ver os multiplos:\n");
     scanf("%d", &num);
 
+    multiplo = 0;
+
     for(i = 0; i < qtdmul; i++)
     {
-        multiplo = num * qtdmul;
-        printf("%d \n", multiplo);
+        multiplo = num * i;
+        printf("%d ", multiplo);
     }
 
     return 0;
