8b86a29f6cdeac67b23737b5f28afd99ab5779fa
thiagobatistagomes <122994560+thiagobatistagomes@users.noreply.github.com>
2023-05-18 11:28:13 -0300

    ex10

---
+++ exercicio10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main ()
+{
+    int N, i, somaDiv;
+
+    do
+    {
+        printf("Digite um numero inteiro positivo:\n");
+        scanf("%d", &N);
+    } while (N < 0);
+
+    for(i = 0; i <= N; i++)
+    {
+        if(N % i == 0)
+        {
+            somaDiv += i;
+        }
+    }
+
+    printf("%d", somaDiv);
+
+    return 0;
+}
